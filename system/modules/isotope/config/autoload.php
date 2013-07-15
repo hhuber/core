982e0ac31ef86951f2fc30254613cb66154c5fde
Yanick Witschi <yanick.witschi@terminal42.ch>
2013-07-15 21:17:15 +0200

    Fixed namespacing once again

--- system/modules/isotope/config/autoload.php
+++ system/modules/isotope/config/autoload.php
@@ -14,6 +14,8 @@
 /**
  * Register PSR-0 namespace
  */
+NamespaceClassLoader::add('Isotope/Gallery', 'system/modules/isotope/library');
+NamespaceClassLoader::add('Isotope/Document', 'system/modules/isotope/library');
 NamespaceClassLoader::add('Isotope', 'system/modules/isotope/library');
 
 
